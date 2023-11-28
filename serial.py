63adc1c4690b2f171ebcd16baf3dc7f3a288eba3
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-28 17:45:54 -0600

    added methods in class serial

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -14,6 +14,31 @@ class SerialGenerator:
 
     >>> serial.reset()
 
-    >>> serial.generate()
+    >>> serial.generate()y
     100
     """
+    def __init__(self, start):
+        """Create serial number starting at a given number(ie.100)."""
+        self.start = start
+        self.count = 0
+
+    def generate(self):
+        """When called, start number increase by 1."""
+        if self.count == 0 :
+            # print("currStartNum=", self.start)
+            return self.start
+        else :
+            self.start += 1
+            # print("nextStartNum=", self.start)
+        self.count += 1
+        return self.startrun
+
+    def reset(self):
+        """Function resets to intial start number."""
+        return self.start - self.count
+
+
+
+
+
+
